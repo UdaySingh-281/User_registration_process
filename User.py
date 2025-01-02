2180213d4e86ad11a92a8b8dd6596264245983b4
UdaySingh-281 <udaysinghspn28122002@gmail.com>
2025-01-02 12:03:11 +0530

    Added UC6

--- User.py
+++ User.py
@@ -3,11 +3,12 @@ from time import sleep
 
 # First name starts with Cap and has minimum 3 characters
 class User_registration:
-  def __init__(self, fname, lname, email, mobile):
+  def __init__(self, fname, lname, email, mobile, password):
     self.fname = fname
     self.lname = lname
     self.email = email
     self.mobile = mobile
+    self.password = password
   
   def check_name(self, name):
     pattern = r'^[A-Z][a-z]{2,}'
@@ -22,8 +23,8 @@ class User_registration:
     return bool(re.match(pattern, mobile))
   
   def check_password(self, password):
-    pattern = r'[a-z]{8,}'
-    return bool(re.match(pattern, password))
+    pattern = r'([A-Za-z]+){8,}'
+    return bool(re.search(pattern, password))
 
   def register(self):
     if self.check_name(self.fname) and self.check_name(self.lname) and self.check_email(self.email) and self.check_mobile(self.mobile) and self.check_password(self.password):
