b2ed1afec8ae27cd5c9397c13fbaaacaf0c9b4a2
UdaySingh-281 <udaysinghspn28122002@gmail.com>
2025-01-02 11:55:42 +0530

    Added UC5

--- User.py
+++ User.py
@@ -20,24 +20,28 @@ class User_registration:
   def check_mobile(self, mobile):
     pattern = r'^[91]+\s[0-9]{10}'
     return bool(re.match(pattern, mobile))
+  
+  def check_password(self, password):
+    pattern = r'[a-z]{8,}'
+    return bool(re.match(pattern, password))
 
   def register(self):
-    if self.check_name(self.fname) and self.check_name(self.lname) and self.check_email(self.email) and self.check_mobile(self.mobile):
+    if self.check_name(self.fname) and self.check_name(self.lname) and self.check_email(self.email) and self.check_mobile(self.mobile) and self.check_password(self.password):
       print("Registration Successful")
       return True
     else:
       print("Registration Failed")
       sleep(2)
       return False
-    
 
 while True:
   print("User Registration Process")
   fname = input("Enter First Name: ")
   lname = input("Enter Last Name: ")
-  email = input("Enter Email: ") 
+  email = input("Enter Email: ")
   mobile = input("Enter Mobile Number: ")
+  password = input("Enter Password: ")
 
-  user = User_registration(fname, lname, email, mobile)
+  user = User_registration(fname, lname, email, mobile, password)
   if user.register():
     break
