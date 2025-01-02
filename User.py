03448cf6d1384ce89370988c66c48d33e983dcf2
UdaySingh-281 <udaysinghspn28122002@gmail.com>
2025-01-02 11:45:53 +0530

    Added UC3

--- User.py
+++ User.py
@@ -3,30 +3,34 @@ from time import sleep
 
 # First name starts with Cap and has minimum 3 characters
 class User_registration:
-    def __init__(self, fname, lname):
-        self.fname = fname
-        self.lname = lname
+  def __init__(self, fname, lname, email):
+    self.fname = fname
+    self.lname = lname
+    self.email = email
   
-    def check_name(self, name):
-        pattern = r'^[A-Z][a-z]{2,}'
-        return bool(re.match(pattern, name))
-  
+  def check_name(self, name):
+    pattern = r'^[A-Z][a-z]{2,}'
+    return bool(re.match(pattern, name))
 
-    def register(self):
-        if self.check_name(self.fname) and self.check_name(self.lname):
-            print("Registration Successful")
-            return True
-        else:
-            print("Registration Failed")
-            sleep(2)
-            return False
+  def check_email(self, email):
+    pattern = r'^[a-z]+([-_.])?[a-z0-9]+@[a-z]+\.[a-z]{,3}([\.a-z]{,3})?$'
+    return bool(re.match(pattern, email))  
+
+  def register(self):
+    if self.check_name(self.fname) and self.check_name(self.lname) and self.check_email(self.email):
+      print("Registration Successful")
+      return True
+    else:
+      print("Registration Failed")
+      sleep(2)
+      return False
 
 while True:
   print("User Registration Process")
   fname = input("Enter First Name: ")
   lname = input("Enter Last Name: ")
-  
+  email = input("Enter Email: ")  
 
-  user = User_registration(fname, lname)
+  user = User_registration(fname, lname, email)
   if user.register():
     break
