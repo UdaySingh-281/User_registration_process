5e3de76b8a881614263f1e228b1bf2decd32bf04
UdaySingh-281 <udaysinghspn28122002@gmail.com>
2025-01-02 11:40:06 +0530

    Added UC2

--- User.py
+++ User.py
@@ -3,26 +3,30 @@ from time import sleep
 
 # First name starts with Cap and has minimum 3 characters
 class User_registration:
-    def __init__(self, fname):
+    def __init__(self, fname, lname):
         self.fname = fname
-
+        self.lname = lname
+  
     def check_name(self, name):
         pattern = r'^[A-Z][a-z]{2,}'
         return bool(re.match(pattern, name))
+  
 
     def register(self):
-        if self.check_name(self.fname):
+        if self.check_name(self.fname) and self.check_name(self.lname):
             print("Registration Successful")
             return True
         else:
             print("Registration Failed")
             sleep(2)
             return False
-        
+
 while True:
   print("User Registration Process")
   fname = input("Enter First Name: ")
+  lname = input("Enter Last Name: ")
+  
 
-  user = User_registration(fname)
+  user = User_registration(fname, lname)
   if user.register():
     break
